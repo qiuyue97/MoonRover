2592d8f08640edf05a1ef39bb62042d2230c132d
Wang <510514908@qq.com>
2024-03-23 15:27:05 +0800

    finish result_saver

--- algorithm/result_saver.py
+++ algorithm/result_saver.py
@@ -6,30 +6,6 @@ import datetime
 import os
 import shutil
 
-file_dicts = [
-    {
-        "file": 'map.npy',
-        "start": [10, -15],
-        "goal": [-15, 15],
-        "points": [[-7.06595, 14.7814], [-16.7138, 20.4243], [-22.0564, 14.6231], [-8.86782, 20.5962], [-13.1897, 9.45819]],
-    },
-    {
-        "file": 'TestMap-3.npy',
-        "start": [11, -14],
-        "goal": [15, 15],
-        "points": [[13.5, 13.5], [16.3, 24], [6.9, 15.2], [10.4, 14.6], [14.8, 14.1]],
-    },
-    {
-        "file": 'TestMap4.npy',
-        "start": [-11, 18],
-        "goal": [15, -15],
-    },
-    {
-        "file": 'TestMap5.npy',
-        "start": [-15, -20],
-        "goal": [0, 10],
-    },
-]
 
 def point_clustering(tar_pos):
     # 移除包含NaN的点
@@ -78,6 +54,7 @@ def point_clustering(tar_pos):
             top_centroids.append(segment_centroids.pop(0))
     return top_centroids
 
+
 def find_closest_point(target_point, points):
     # 计算目标点和每个点的距离
     distances = np.sqrt(np.sum((points - target_point)**2, axis=1))
@@ -86,53 +63,39 @@ def find_closest_point(target_point, points):
     min_distance = distances[min_distance_index]
     return min_distance_index, min_distance
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# points = np.load('./real_map.npy')
-# elevation = np.load('./map_handled.npy')
-# print(elevation.shape)
-#
-# # 计算x、y、z坐标的最小值和最大值
-# x_min, z_min, y_min = np.min(points, axis=0)
-# x_max, z_max, y_max = np.max(points, axis=0)
-# print(f'x: min={x_min}, max={x_max}, x shape is: {x_max - x_min}')
-# print(f'y: min={y_min}, max={y_max}, y shape is: {y_max - y_min}')
-# print(f'z: min={z_min}, max={z_max}, z shape is: {z_max - z_min}')
-#
-# # 计算先验地图中高程的最小值和最大值
-# e_min = np.min(elevation[0])
-# e_max = np.max(elevation[0])
-# print(f'e: min={e_min}, max={e_max}, e shape is: {e_max - e_min}')
-# print(f'高程地图的数据应该除以{(e_max - e_min) / (z_max - z_min)}才能变成以m为单位。')
 
-# # 创建一个PointCloud对象
-# pcd = o3d.geometry.PointCloud()
-#
-# # 将坐标添加到PointCloud对象中
-# pcd.points = o3d.utility.Vector3dVector(points)
-#
-# # 可视化点云
-# o3d.visualization.draw_geometries([pcd])
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# moon_map = np.load('map.npy')
-# map_np = moon_map[:, :, 3] / 1.417379975200399
-# np.save('map_np', map_np)
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# tar = np.array([-15, 15])
-# now = np.array([-14.61865831, 14.98553827])
-# distances = np.sqrt(np.sum((tar - now)**2))
-# print(distances)
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# target_pos = [[-16.704171486252157, 10.23946787762889], [-18.177401720334355, 17.209447349706153]]
-# for tar in target_pos:
-#     min_distance_index, min_distance = find_closest_point(tar)
-#     print(f"Closest point index: {min_distance_index + 1}, Distance: {min_distance}")
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-# 创建点集
-
-def result_saver(real_pos, additional_points):
+def result_saver(map_id, tar_path):
+    file_dicts = [
+        {
+            "file": 'map.npy',
+            "start": [10, -15],
+            "goal": [-15, 15],
+            "points": [[-7.06595, 14.7814], [-16.7138, 20.4243], [-22.0564, 14.6231], [-8.86782, 20.5962],
+                       [-13.1897, 9.45819]],
+        },
+        {
+            "file": 'TestMap-3.npy',
+            "start": [11, -14],
+            "goal": [15, 15],
+            "points": [[13.5, 13.5], [16.3, 24], [6.9, 15.2], [10.4, 14.6], [14.8, 14.1]],
+        },
+        {
+            "file": 'TestMap4.npy',
+            "start": [-11, 18],
+            "goal": [15, -15],
+            "points": [[10.325, -16.71], [23.0538, -11.1559], [16.9515, -7.44492], [12.0482, -9.68678], [13.43, -17.8119]],
+        },
+        {
+            "file": 'TestMap5.npy',
+            "start": [-15, -20],
+            "goal": [0, 10],
+            "points": [[1, 15], [4.1, 14.6], [-5, 13.2], [-1.6, 18.7], [-1.3, 8.6]],
+        },
+    ]
+    real_pos = np.array(file_dicts[map_id]['goal']).astype(np.float64)
+    additional_points = file_dicts[map_id]['points']
     counter = [0, 0, 0, 0, 0]
-    points = np.load('./temp/tar_all.npy')
+    points = np.load(tar_path)
     target_pos = point_clustering(points)
     for tar in target_pos:
         min_distance_index, min_distance = find_closest_point(tar, np.array(additional_points))
@@ -165,25 +128,18 @@ def result_saver(real_pos, additional_points):
     sm = plt.cm.ScalarMappable(cmap="Blues", norm=norm)
     sm.set_array([])
     plt.colorbar(sm)
-    # plt.show()
     # 获取当前时间
     now = datetime.datetime.now()
     # 格式化为月_日_时_分
     time_str = now.strftime("%m_%d_%H_%M")
     # 指定保存路径和文件名
-    save_path = os.path.join("./result", f"{time_str}.png")
+    save_path = os.path.join("./algorithm/result", f"map{map_id}_{time_str}.png")
     plt.savefig(save_path)
-    # # 打开 log.txt 文件并添加一行文字
-    # with open('./result/log_2_3.txt', 'a', encoding='utf-8') as log_file:
-    #     log_file.write(f'{now.strftime("%m-%d %H:%M")}小车最终落于{real_pos},与目标区域中心相距{distance.euclidean(real_pos, [-15, 15])}米，发现的兴趣点：{counter}。\n')
-    # shutil.copyfile('./algorithm/temp/tar_all.npy', f'./algorithm/result/tar/{now.strftime("%m_%d_%H_%M")}.npy')
-    # # 指定要删除的目录
-    # dir_path = "./algorithm/temp"
-    # # 使用shutil.rmtree删除指定目录及其所有内容
-    # shutil.rmtree(dir_path, ignore_errors=True)
+    # 打开 log.txt 文件并添加一行文字
+    with open('./algorithm/result/log.txt', 'a', encoding='utf-8') as log_file:
+        log_file.write(f'{now.strftime("%m-%d %H:%M")}地图{map_id}中，发现的兴趣点：{counter}。\n')
+    shutil.copyfile('./algorithm/temp/tar_all.npy', f'./algorithm/result/tar/map{map_id}_{time_str}.npy')
 
 
-map_id = 1
-goal = file_dicts[map_id]['goal']
-result_saver(np.array(goal).astype(np.float64), file_dicts[map_id]['points'])
-
+if __name__ == '__main__':
+    result_saver(3, './temp/tar_all.npy')
